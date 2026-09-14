291eba7e8ec657f20c0edb572ddd08e3141f96bd
ASHOK DIGITAL SERVICES <ashokds.com@gmail.com>
2026-09-14 19:30:59 +0530

    Add files via upload

---
+++ README-FINAL-CMS.txt
@@ -0,0 +1,4 @@
+DEVI GROUPS FINAL CMS PACKAGE
+Admin homepage content field fixed: site_content.content
+Supabase CMS config included.
+Product + Business Unit live integration included.
--- cms-config.js
+++ cms-config.js
@@ -1,4 +1,4 @@
 window.DEVI_CMS_CONFIG = {
   supabaseUrl: "https://bgkymxdbmvbplnlehakd.supabase.co",
-  supabaseKey: "sb_publishable_J3m0j2EknLIDDQW9ZRLJ-Q_FxOlUFN7"
+  supabaseKey: "sb_publishable_J3m0j2EknLIDDQW9ZRL-JQ_FxOlUFN7"
 };
