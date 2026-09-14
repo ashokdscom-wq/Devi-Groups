5121ec91846df3139c20ce9e5b795a45f67fd0c3
ASHOK DIGITAL SERVICES <ashokds.com@gmail.com>
2026-09-14 14:01:23 +0530

    Update cms-config.js

--- cms-config.js
+++ cms-config.js
@@ -1,1 +1,4 @@
-<script src="../cms-config.js"></script>
+window.DEVI_CMS_CONFIG = {
+  supabaseUrl: "https://bgkymxdbmvbplnlehakd.supabase.co",
+  supabaseKey: "sb_publishable_J3m0j2EknLIDDQW9ZRLJ-Q_FxOlUFN7"
+};
